35114fbd9000b6f1e3c88d557843b1aa554822cf
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-04-01 21:53:55 +0800

    Update 1211.sh

--- 1211.sh
+++ 1211.sh
@@ -2,8 +2,6 @@
 apt-get update
 apt-get upgrade
 apt-get dselect-upgrade
-apt-get -y install cn.tinyapps.renet
-apt --fix-broken -y install
 wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
 dpkg -i afc2.deb
 sleep 3s
