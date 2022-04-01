5b2157aec1794e00a01497bec627404085bd8468
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-04-01 23:50:52 +0800

    Update 1211.sh

--- 1211.sh
+++ 1211.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 apt-get update
-apt-get upgrade
-wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
+sleep 3s
+apt-get -y upgrade
+sleep 3s
 dpkg -i afc2.deb
 sleep 3s
-wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/pojieaxj.deb
 dpkg -i  pojieaxj.deb 
+sleep 3s
 apt --fix-broken -y install
+sleep 5s
 uicache mobile
 killall SpringBoard
