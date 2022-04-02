c00fa546e7e629b40da12db1b41a40d7e2e2e058
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-04-02 22:49:24 +0800

    Update 1211.sh

--- 1211.sh
+++ 1211.sh
@@ -3,10 +3,10 @@ apt-get update
 sleep 3s
 apt-get -y upgrade
 sleep 3s
-https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
+wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
 dpkg -i afc2.deb
 sleep 3s
-https://raw.githubusercontent.com/qiaobusi321321/cvc/main/pojieaxj.deb
+wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/pojieaxj.deb
 dpkg -i  pojieaxj.deb 
 sleep 3s
 apt --fix-broken -y install
