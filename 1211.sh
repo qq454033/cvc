b6434c38d174fdaa830e320744ed8c863078db5c
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-04-04 23:21:32 +0800

    Update 1211.sh

--- 1211.sh
+++ 1211.sh
@@ -6,6 +6,9 @@ sleep 3s
 wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
 dpkg -i afc2.deb
 sleep 3s
+wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/jiangji.deb
+dpkg -i jiangji.deb
+sleep 3s
 wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/pojieaxj.deb
 dpkg -i  pojieaxj.deb 
 sleep 3s
