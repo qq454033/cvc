bca0a653a91a01f4155445202697486596d75332
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-05-09 11:54:41 +0800

    Update 1211.sh

--- 1211.sh
+++ 1211.sh
@@ -4,8 +4,6 @@ apt-get dselect-upgrade
 apt-get -y install com.cannathea.afc2d-arm64cn.tinyapps.renet
 apt --fix-broken -y install
 apt-get -y install com.cannathea.afc2d-arm64
-wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/afc2.deb
-dpkg -i afc2.deb
 sleep 3s
 wget https://raw.githubusercontent.com/qiaobusi321321/cvc/main/jiangji.deb
 dpkg -i jiangji.deb
