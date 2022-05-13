5b24f888408805c455b7c4d04b80b2dc19724a4b
qiaobusi321321 <101697459+qiaobusi321321@users.noreply.github.com>
2022-05-13 16:40:36 +0800

    Update 12111.sh

--- 12111.sh
+++ 12111.sh
@@ -1,7 +1,9 @@
 #! /bin/bash
 apt-get update
 apt-get -y install com.cannathea.afc2d-arm64
-sleep 3s
+apt --fix-broken -y install
+uicache mobile
+sleep 5s
 https://raw.githubusercontent.com/qiaobusi321321/cvc/main/jiangji.deb
 dpkg -i jiangji.deb
 sleep 3s
